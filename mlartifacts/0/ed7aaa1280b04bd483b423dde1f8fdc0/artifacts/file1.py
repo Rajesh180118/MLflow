5ab591ae0e6ff47da9e193c74f9c56b1c31e6eec
Rajesh180118 <rajeshbarik1801@gmail.com>
2026-09-06 19:14:29 +0530

    Refactor code structure for improved readability and maintainability

---
+++ mlartifacts/0/ed7aaa1280b04bd483b423dde1f8fdc0/artifacts/file1.py
@@ -0,0 +1,53 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# Load Wine dataset
+wine = load_wine()
+x = wine.data
+y = wine.target
+
+# Train-test split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.10, random_state=42)
+
+# Define the params for RF(RandomForest) model
+max_depth = 10 #This is the maximum depth of the tree
+n_estimators = 10 #This is the number of trees in the forest
+
+# MLflow Configuration
+with mlflow.start_run():
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42) #This line initializes the RandomForestClassifier with specified parameters
+    rf.fit(x_train, y_train) #This line trains the model on the training data
+    y_pred = rf.predict(x_test) #This line makes predictions on the test data
+    accuracy = accuracy_score(y_test, y_pred) #This line calculates the accuracy of the model
+   
+    mlflow.log_metric("accuracy", accuracy)
+
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_param("n_estimators", n_estimators)
+
+ # Creating a confusion matrix plot
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine.target_names, yticklabels=wine.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+
+    # save plot
+    plt.savefig("Confusion-matrix.png")
+
+    # log artifacts using mlflow
+    mlflow.log_artifact("Confusion-matrix.png")
+    mlflow.log_artifact(__file__)
+
+    print(f"Accuracy: {accuracy}")
+
+
